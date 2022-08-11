3493a903819170899898b16f4238e680fd46f6da
Tony Herbert <tonyherbert22@gmail.com>
2022-08-11 13:20:58 -0500

    add an edit button

--- app.js
+++ app.js
@@ -29,13 +29,13 @@ if (process.env.NODE_ENV === "development") {
 }
 
 // Handlebars HELPERS
-const { formatDate } = require("./helpers/hbs");
+const { formatDate, stripTags, truncate, editIcon } = require("./helpers/hbs");
 
 // Handlebars
 app.engine(
     ".hbs",
     exphbs.engine({
-        helpers: { formatDate },
+        helpers: { formatDate, stripTags, truncate, editIcon },
         defaultLayout: "main",
         extname: ".hbs",
     })
@@ -57,6 +57,12 @@ app.use(
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Set global var
+app.use(function(req,res, next){
+    res.locals.user = req.user || null
+    next()
+})
 
 // Static Folder
 app.use(express.static(path.join(__dirname, "public")));
